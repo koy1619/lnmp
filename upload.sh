802b0e34c54d13faa55870310965ae46021b9b03
koy1619 <maxiaolei007@sina.com>
2020-08-06 10:35:12 +0800

    upupdate:smiling_imp::ok::ok_hand:

--- upload.sh
+++ upload.sh
@@ -1,5 +1,11 @@
-git init
+#一键提交
+#git init
+
+git pull
+
+echo "请输入本次提交的说明:"
+read description
 git add -A
-git commit -m "add new" .
-git remote set-url origin git@github.com:koy1619/lnmp.git
+git commit -m "$description:smiling_imp::ok::ok_hand:" .
+git remote add origin git@github.com:koy1619/lnmp.git
 git push -u origin master
